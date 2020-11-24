86f39a36421906a59f5e6057808aa238b730e825
uwase-diane <diane.uwase13@gmail.com>
2020-11-24 05:15:51 -0500

    created a class source

---
+++ app/models/source.py
@@ -0,0 +1,12 @@
+class Source:
+
+    def __init__(self,id,name,category,description,url,language):
+
+        self.id = id
+        self.name = name
+        self.category = category
+        self.description = description
+        self.url = url
+        self.language = language
+
+        
